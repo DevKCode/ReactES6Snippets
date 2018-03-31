e6080c5c17b751bb48291e1e070d9fb102747851
Dev Kumar <y2ksoft@gmail.com>
2018-03-31 23:56:15 +1300

    List Update

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,7 @@ const person = (props) => {
   return (
      <div>
      <p onClick={props.click}> I am a {props.name} &  I am  {props.age} year odl</p>
+     <input type="text" onChange={props.changed}/>
      
      </div>  
     );
